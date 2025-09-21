903682e6968cef456a824c1102faa562400c00f0
Julia <julia021216@gmail.com>
2025-09-21 21:13:37 +0200

    Basic Setup
    Added:
    1. CharacterStats template
    2. Added character to a test-scene
    
    Stopped video at 7:55 (link in folder)

---
+++ src/characters/CharacterStats.gd
@@ -0,0 +1,42 @@
+# Numerically represents stats to all instances of the class [Character]
+
+class_name CharacterStats extends Resource
+
+# Non-modifiable aspects of Character
+@export var character_name : String = "John"
+@export var battle_class_type : String = "Class"
+
+# Character Stats
+@export var curr_health : int
+@export var max_health : int
+@export var phys_attack : int
+@export var mag_attack : int  
+@export var phys_defense : int
+@export var mag_defense : int
+@export var crit_chance : int
+@export var level : int
+@export var mobility : int
+
+# List of all stats that can be modified
+const MODIFIABLE_STATS = [
+	"curr_health", "max_health", "phys_attack", "mag_attack", "phys_defense", "mag_defense", "crit_chance", "level", "mobility"
+]
+
+# Set-functions
+func set_max_health(value):
+	max_health = max(0, value)
+
+# Basic combat functions
+func take_damage(hit):
+	curr_health -= hit.damage
+	curr_health = max(0, curr_health)	# Make sure health is 0 or more
+	
+	emit_signal("health_changed", curr_health)
+	
+	if(curr_health == 0):	# Death
+		emit_signal("health_depleated")
+		
+func recieve_healing(amount):
+	curr_health += amount
+	curr_health = min(curr_health, max_health)
+	emit_signal("health_changed", amount)
