1af68cbc2b8f39168b11da3a30974584a99f95a2
RainbowSapling <mirbj463@student.liu.se>
2025-10-17 23:07:35 +0200

    Basic level selection created

---
+++ src/Level/GUI/level_button.gd
@@ -0,0 +1,19 @@
+@tool
+extends Button
+
+signal level_selected
+
+@export var level_num: int = 1
+@export var locked: bool = true:
+	set(value):
+		locked = value
+		level_locked() if locked else level_unlocked()
+		
+func level_locked():
+	disabled = true
+	
+func level_unlocked():
+	disabled = false
+
+func _on_pressed() -> void:
+	level_selected.emit(level_num) # Signal used to specify which level was selected
---
+++ src/Level/GUI/level_select.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var level_container: HBoxContainer = $LevelContainer
+
+func _ready() -> void:
+	setup_level_button()
+	connect_selected_level_to_level_button()
+	
+func setup_level_button():
+	for button in level_container.get_children():
+		button.level_num = button.get_index() + 1 # Assign level number to the button
+		button.text = "Level " + str(button.level_num) # Update text on button (Level number)
+		button.locked = true # All levels are originally locked
+	level_container.get_child(0).locked = false # First level is unlocked
+
+func connect_selected_level_to_level_button():
+	for button in level_container.get_children():
+		button.connect("level_selected", change_to_scene) # Sends level_num from button to change_to_scene
+		
+func change_to_scene(level_num: int):
+	var next_level: String = "res://src/Level/Levels/Level_" + str(level_num) + ".tscn" # Filepath to selected level
+	
+	# If the filepath is valid, change scene to the selected level
+	if FileAccess.file_exists(next_level):
+		get_tree().change_scene_to_file(next_level)
