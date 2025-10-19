bc981c8fef627a507d587afc5d6d871c3e4850fe
RainbowSapling <mirbj463@student.liu.se>
2025-10-19 19:41:23 +0200

    win/loss screen started

--- src/Level/win_loss_condition.gd
+++ src/Level/win_loss_condition.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @onready var character_manager: Node2D = $"../TileMapLayer/CharacterManager"
+@onready var victory_screen: PanelContainer = $"../GUI/Margin/VictoryScreen"
+@onready var game_over_screen: PanelContainer = $"../GUI/Margin/GameOverScreen"
+
+
 
 func check_conditions():
 	# If all enemy characters are dead -> win
@@ -10,10 +14,14 @@ func check_conditions():
 	# If all player characters are dead -> lose
 	if character_manager.player_list.is_empty():
 		lose()
+		
+	# Custom conditions can be added as child nodes and therefor be unique per level
 	
 
 func win():
 	print("victory :3")
+	victory_screen.show()
 	
 func lose():
 	print("game over :(")
+	game_over_screen.show()
