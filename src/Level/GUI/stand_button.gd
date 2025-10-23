68954e8bb84eead5728c5123b64491a801730cff
Julia <julia021216@gmail.com>
2025-10-23 17:20:51 +0200

    Refined stand-button (not important))

--- src/Level/GUI/stand_button.gd
+++ src/Level/GUI/stand_button.gd
@@ -1,12 +1,13 @@
 extends Button
+
+"""
+# This file contains protocol for unit passing its turn.
+"""
 @onready var character_manager: Node2D = $"../../../../../TileMapLayer/CharacterManager"
 @onready var turn_manager: Node2D = $"../../../../../TileMapLayer/TurnManager"
-
-
 
 var counter: int = 0
 
 func _pressed() -> void:
-	
 	var current = character_manager.current_character
 	turn_manager.end_player_unit_turn(current)
