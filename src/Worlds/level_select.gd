5455df1ac6b91b8b850c711ebef2506024ceeacc
RainbowSapling <mirbj463@student.liu.se>
2025-10-30 23:44:50 +0100

    Boss music

--- src/Worlds/level_select.gd
+++ src/Worlds/level_select.gd
@@ -3,12 +3,14 @@ extends Control
 @onready var level_container: HBoxContainer = $LevelContainer
 @onready var world_handler: Node = $WorldHandler
 
+var level_music: String
 var world_num: int
 
 func _ready() -> void:
 	setup_level_button()
 	connect_selected_level_to_level_button()
 	world_num = world_handler.world_script.current_world
+	level_music = "Battle"
 	
 func setup_level_button():
 	for button in level_container.get_children():
@@ -33,10 +35,14 @@ func change_to_scene(level_num:int):
 	var next_level: String = ("res://src/Worlds/World_" + str(world_num) + 
 	"/Levels/Level_" + str(world_num) + "-" + str(level_num) + ".tscn") # Filepath to selected level
 
+	# Check if last level
+	if level_num == 5 and world_handler.world_script.current_world == 2:
+		level_music = "Boss"
+
 	# If the filepath is valid, change scene to the selected level
 	if FileAccess.file_exists(next_level):
 		get_tree().change_scene_to_file(next_level)
 		world_handler.world_script.set_current_level(level_num)
 		world_handler._save_game()
-		AudioManager.update_music_for_scene("Battle")
+		AudioManager.update_music_for_scene(level_music)
 	
